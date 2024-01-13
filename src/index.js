7e048154a8ac572581f5a6308898d29020a37971
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-01-13 13:17:14 +0530

    Working on user and video models

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // require('dotenv').config({path:".env"});
 import dbConnect from './db/dbase.js';
 import dotenv from "dotenv";
-
+import app from "./app.js"
 dotenv.config({
     path:'./env'
 })
 
-dbConnect();
+dbConnect()
+.then(()=>app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is running at PORT ${process.env.PORT}`);
+}))
+.catch((err)=>console.log("Error",err));
---
+++ src/models/users.model.js
@@ -0,0 +1,87 @@
+import mongoose,{Schema, model} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema=new Schema({
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    username:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    avatar:{
+        type:String, //Cloudinary Url
+        required:true,
+    },
+    coverImage:{
+        type:String, ////Cloudinary Url
+        required:true,
+    },
+    password:{
+        type:String,
+        required:[true,'Password is Required'],
+    },
+    refreshToken:{
+        type:String,
+    },
+},{timestamps:true})
+
+//encrypting the password
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")) return next()
+
+    this.password=bcrypt.hash(this.password,10),
+    next()
+})
+
+//checking if password is correct
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare("password",this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    jwt.sign({
+        _id:this._id,
+        fullName:this.fullName,
+        email:this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+    })
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+    })
+}
+
+export const Users=model("Users",userSchema)
