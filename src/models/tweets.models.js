95a35bbd70571a3d4f52c5dee87cc6a7c99c8f4f
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-01-20 02:58:17 +0530

    Added comment, like, playlist, and tweet models

---
+++ src/models/tweets.models.js
@@ -0,0 +1,14 @@
+import mongoose,{Schema} from "mongoose"
+
+const tweetSchema=new Schema({
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"user",
+    },
+    content:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true})
+
+export const tweet=model("tweet",tweetSchema)
